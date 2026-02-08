4c5fff156a02b32da440ae3f6e665641b632ae08
barrfalk <bfalk@salussystems.com>
2026-02-08 11:58:35 -0800

    Update actions.js

--- backend/api/helpers/actions.js
+++ backend/api/helpers/actions.js
@@ -1,72 +1,72 @@
-'use strict';
-const _ = require('lodash');
-const defaultLog = require('./logger')('actions');
+'use strict'
+const _ = require('lodash')
+const defaultLog = require('./logger')('actions')
 
 exports.publish = function(o) {
   return new Promise(function(resolve, reject) {
     var exists = _.find(o.tags, function(item) {
-      return _.isEqual(item, ['public']);
-    });
+      return _.isEqual(item, ['public'])
+    })
 
-    // Object was already published?
+    // Object was already published ?
     if (exists) {
-      defaultLog.info('HTTP 409, Object already published:', exists);
+      defaultLog.info('HTTP 409, Object already published:', exists)
       reject({
         code: 409,
-        message: 'Object already published'
-      });
+        message: 'Object already published',
+      })
     } else {
       // Add publish, save then return.
-      o.tags.push(['public']);
+      o.tags.push(['public'])
       o.save().then(resolve, function(err) {
-        reject({ code: 400, message: err.message });
-      });
+        reject({ code: 400, message: err.message })
+      })
     }
-  });
-};
+  })
+}
 
 exports.isPublished = function(o) {
   return _.find(o.tags, function(item) {
-    return _.isEqual(item, ['public']);
-  });
-};
+    return _.isEqual(item, ['public'])
+  })
+}
 
 exports.unPublish = function(o) {
   return new Promise(function(resolve, reject) {
     var exists = _.remove(o.tags, function(item) {
-      return _.isEqual(item, ['public']);
-    });
+      return _.isEqual(item, ['public'])
+    })
     // Object wasn't already published?
     if (exists.length === 0) {
-      defaultLog.info('HTTP 409, Object already unpublished:', exists);
+      defaultLog.info('HTTP 409, Object already unpublished:', exists)
       reject({
         code: 409,
-        message: 'Object already unpublished'
-      });
+        message: 'Object already unpublished',
+      })
     } else {
-      o.markModified('tags');
+      o.markModified('tags')
       // Remove publish, save then return.
       o.save().then(resolve, function(err) {
-        reject({ code: 400, message: err.message });
-      });
+        reject({ code: 400, message: err.message })
+      })
     }
-  });
-};
+  })
+}
 
 exports.delete = function(o) {
   return new Promise(function(resolve, reject) {
     _.remove(o.tags, function(item) {
-      return _.isEqual(item, ['public']);
-    });
-    o.isDeleted = true;
-    o.markModified('tags');
-    o.markModified('isDeleted');
+      return _.isEqual(item, ['public'])
+    })
+    o.isDeleted = true
+    o.markModified('tags')
+    o.markModified('isDeleted')
     // save then return.
     o.save().then(resolve, function(err) {
-      reject({ code: 400, message: err.message });
-    });
-  });
-};
+      reject({ code: 400, message: err.message })
+    })
+  })
+}
 
 /**
  * Sends an http response.
@@ -79,7 +79,7 @@ exports.delete = function(o) {
  * @returns {*} res an http response
  */
 exports.sendResponse = function(res, code, object) {
-  const httpErrorCode = code || (object && object.code) || 500;
-  res.writeHead(httpErrorCode, { 'Content-Type': 'application/json' });
-  return res.end(JSON.stringify(object));
-};
+  const httpErrorCode = code || (object && object.code) || 500
+  res.writeHead(httpErrorCode, { 'Content-Type': 'application/json' })
+  return res.end(JSON.stringify(object))
+}
